6ba5d4a0f869bbc16a665e2763db53a9a93c87de
Shivansh Anand <shivanshanandisminame@outlook.com>
2021-03-06 15:14:32 +0530

    added anims ( with vue transitions )

--- vue_todo/vue-todo.js
+++ vue_todo/vue-todo.js
@@ -1,73 +1,56 @@
-let vm = new Vue({
-    el  : "#main",
-    data : {   
-        currText : "",
-        items : []
-    },
-    methods : {
-        addItem : function() {
-            if(this.currText.length <= 0) {
-                showToast("Can't add an empty item :(" , 1000);
-                return;
-            }
-            let item = {
-                cc : new Date().getTime(),
-                text : this.currText,
-                completed : false,
-                disabled : true,
-                mClass : {
-                    "mark-complete" : false,
-                },
-                btnToggle : true
-            }
-            this.items.push(item);
-            this.currText = "";
-            showToast("New item added !" , 1000);
-        },
-        removeItem : function(i) {
-            this.items.splice(i , 1);
-            showToast("Item deleted !" , 1000);
-        },
-        markAsCompleted : function(i , ev) {
-            let e = this.items[i];
-            e.completed = !e.completed;
-            e.mClass["mark-complete"] = !e.mClass["mark-complete"];
-            e.btnToggle = !e.btnToggle;
-            showToast(e.completed ? "Task completed !" : "Undoing ..." , 1000);
-        },
-        makeEditable : function(i) {
-            let e = this.items[i];
-            e.disabled = !e.disabled;
-        },
-        makeStatic : function(i) {
-            let e = this.items[i];
-            e.disabled = !e.disabled;
-        }
-    },
-    computed : {
-        placeholder : function() {
-            return `Write your todo #${this.items.length + 1}`;
-        }
-    }
-});
+<html>
 
-let vm2 = new Vue({
-    el : "#info-btn",
-    methods : {
-        clicked : function() {
-            let msg = "Created by Shivansh Anand <br />Using it is self-explanatory , if you want to edit a todo just double click on it";
-            showToast(msg , 3000);
-        }
-    }
-});
+    <head>
 
-function showToast(msg, time) {
-    let t = document.getElementById("toast");
-    t.querySelector("p").innerHTML = msg;
-    t.classList.remove("op-0");
-    t.classList.add("op-1");
-    setTimeout(() => {
-        t.classList.remove("op-1");
-        t.classList.add("op-0");
-    }, time + 1000);
-}
+        <meta name= "viewport" content= "width=device-width initial-scale=1" />
+        <link href = "vue-todo.css" rel = "stylesheet"  />
+        <script defer src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+        <script defer type = "text/javascript" src = "vue-todo.js"></script>
+
+    </head>
+
+    <body>
+
+        <main id = "main">
+
+            <h1>Vue Todo</h1>
+
+            <div id = "inp-container">
+
+                <button v-on:click = "addItem"></button>
+                <input type = "text" v-bind:placeholder = "placeholder" v-model.lazy = "currText" v-on:keyup.enter = "addItem"/>
+
+            </div>
+
+            <p v-if = "items.length < 1">Try adding something !</p>
+
+            <ul>
+
+                <transition-group name = "fade">
+                    <li v-for = "(e , i) of items" v-bind:key = "e.cc">
+
+                        <div v-bind:class = "e.mClass" tabindex="-1" v-on:dblclick = "makeEditable(i)">
+                            <button v-on:click = "markAsCompleted(i , $event)" v-bind:class = "{'checked' : e.btnToggle , 'undo' : !e.btnToggle}"></button>
+                            <input type = "text" v-bind:disabled = "e.disabled" v-on:blur = "makeStatic(i)" v-model.lazy = "e.text"/>
+                            <button v-on:click = "removeItem(i)">&#10006;</button>
+                        </div>
+
+                    </li>  
+                </transition-group>
+
+            </ul>
+
+
+        </main>
+
+        <div id = "toast" class="op-0">
+            <p>Some msg in here</p>
+        </div>
+
+        <a id = "info-btn" href = "#" v-on:click.prevent = "clicked">
+
+        </a>
+
+    </body>
+
+</html>
